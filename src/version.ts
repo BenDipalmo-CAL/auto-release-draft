03a67f6b67847d03518a8f202fdf7a10428a8755
BenDipalmo-CAL <ben.dipalmo@calastone.com>
2024-08-02 16:43:24 +0100

    remove semver

--- src/version.ts
+++ src/version.ts
@@ -1,15 +1,18 @@
- // @ts-expect-error cannot use require and the import throws a build error
-import * as semver from 'semver'
+
+// import * as semver from 'semver'
 
 export function isSemVer(version: string): boolean {
-  return semver.valid(version) != null
+  // return semver.valid(version) != null
+  return true;
 }
 
 export function isPreRelease(version: string): boolean {
-  return semver.prerelease(version) != null
+  // return semver.prerelease(version) != null
+  return false;
 }
 
 export function removePrefix(version: string): string {
-  const parsedVersion = semver.valid(version)
-  return parsedVersion ? parsedVersion : version
+  // const parsedVersion = semver.valid(version)
+  // return parsedVersion ? parsedVersion : version
+  return version;
 }
